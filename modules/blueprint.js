8ecca249abcc8f6ff4760ef3a3ee57486a6bdffe
rbournissent <rodrigobourni@hotmail.com>
2016-01-29 18:07:04 -0300

    Remove log and Prefix Authorization token with 'Basic '
    Update your env vars

--- modules/blueprint.js
+++ modules/blueprint.js
@@ -1,9 +1,7 @@
 var BlueprintClient = require('xively-blueprint-client-js');
 
 var client = new BlueprintClient({
-  authorization: process.env.BLUEPRINT_AUTHORIZATION
+  authorization: 'Basic ' + process.env.BLUEPRINT_AUTHORIZATION
 });
-
-console.log('Client initialized with authorization >', process.env.BLUEPRINT_AUTHORIZATION);
 
 module.exports = client;
