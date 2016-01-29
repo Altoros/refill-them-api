e8c621a87d93590901e128ab8e2fb8b649b63270
rbournissent <rodrigobourni@hotmail.com>
2016-01-29 18:01:05 -0300

    Add logging with Blueprint authorization token

--- modules/blueprint.js
+++ modules/blueprint.js
@@ -4,4 +4,6 @@ var client = new BlueprintClient({
   authorization: process.env.BLUEPRINT_AUTHORIZATION
 });
 
+console.log('Client initialized with authorization >', process.env.BLUEPRINT_AUTHORIZATION);
+
 module.exports = client;
